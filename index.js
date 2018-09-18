5a70f04be1edf363d12c9e3d82cb6aeb6da73b0f
David Yim <yimd85@gmail.com>
2018-09-18 18:13:59 -0400

    added axios

--- index.js
+++ index.js
@@ -1,40 +1,26 @@
 const express = require('express');
-const https = require("https");
-
+const axios = require("axios");
 require('dotenv').config()
 
 const app = express();
 
+const username = process.env.DB_USER;
+const password = process.env.DB_PASSWORD;
 
-var username = process.env.DB_USER;
-var password = process.env.DB_PASSWORD;
-
-
-var auth = "Basic " + new Buffer(username + ':' + password).toString('base64');
+const auth = "Basic " + new Buffer(username + ':' + password).toString('base64');
 
 app.get('/', (request, response) => {
-    var request = https.request({
-        method: "GET",
-        host: "api.intrinio.com",
-        path: "/data_point?identifier=CL&item=cashandequivalents,totalliabilities,weightedavedilutedsharesos,adj_close_price",
-        headers: {
-            "Authorization": auth
-        }
-    }, function(response) {
-        var json = "";
-        response.on('data', function (chunk) {
-            json += chunk;
-        });
-        response.on('end', function() {
-            var company = JSON.parse(json);
-            console.log(company);
-        });
-    });
-    
-    request.end();
 
-
-    response.send(response.data)
+    axios.get("https://api.intrinio.com/data_point?identifier=CL&item=cashandequivalents,totalliabilities,weightedavedilutedsharesos,adj_close_price",
+    {
+        headers: {"Authorization": auth}
+    }).then(function (response) {
+        // handle success
+        console.log(response.data);
+    }).catch(function (error) {
+        // handle error
+        console.log(error);
+    })
 });
 
 app.listen(8000, () => {

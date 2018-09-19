27f8d103d883785faf726f18feb1afd18c62ba94
David Yim <yimd85@gmail.com>
2018-09-19 08:08:42 -0400

    updated eslint and made updates

--- index.js
+++ index.js
@@ -1,34 +1,32 @@
 const express = require('express');
-const axios = require("axios");
-require('dotenv').config()
+const axios = require('axios');
+require('dotenv').config();
 
 const app = express();
 
 const username = process.env.DB_USER;
 const password = process.env.DB_PASSWORD;
 
-const auth = "Basic " + new Buffer(username + ':' + password).toString('base64');
+const auth = 'Basic ' + new Buffer(username + ':' + password).toString('base64');
 
 app.get('/', (request, response) => {
-    axios.get(`https://api.intrinio.com/data_point?identifier=FLWS,SRCE,FOXA,FOX&item=cashandequivalents,totalliabilities,weightedavedilutedsharesos,adj_close_price`,
+    axios.get('https://api.intrinio.com/data_point?identifier=FLWS,SRCE,FOXA,FOX&item=cashandequivalents,totalliabilities,weightedavedilutedsharesos,adj_close_price',
     {
-        headers: {"Authorization": auth}
-    }).then(function (response) {
+        headers: {'Authorization': auth}
+    }).then(() => {
         // handle success
         console.log(response.data.data);
-    }).catch(function (error) {
+    }).catch(error => {
         // handle error
         console.log(error);
-    })
+    });
 
     response.send('x');
 });
 
 app.listen(8000, () => {
     console.log('listening on port 8000');
-});dfdfd
-
-
+});
 
 
 // "https://api.intrinio.com/data_point?identifier=CL&item=cashandequivalents,totalliabilities,weightedavedilutedsharesos,adj_close_price"

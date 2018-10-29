bc0863de401ff878c794e69ef05650b0a900e328
David Yim <yimd85@gmail.com>
2018-10-28 21:50:45 -0400

    added new things

--- index.js
+++ index.js
@@ -4,8 +4,8 @@ require('dotenv').config();
 
 const app = express();
 
-const username = process.env.DB_USER;
-const password = process.env.DB_PASSWORD;
+const username = process.env.DB_USERJaeL;
+const password = process.env.DB_PASSWORDJaeL;
 
 const x = require('./stocks');
 
@@ -36,6 +36,7 @@ app.get('/filinginformation', async (request, response) => {
 
 app.get('/cash', async (request, response) => {
                     for (let i = 0; i < x.russell3000D.length; i++) { 
+                
                         await axios.get(`https://api.intrinio.com/data_point?identifier=${x.russell3000D[i]}&item=cashandequivalents,totalliabilities,weightedavedilutedsharesos,adj_close_price`,
                             {
                                 headers: {'Authorization': auth}
@@ -50,6 +51,22 @@ app.get('/cash', async (request, response) => {
                     }    
                 response.send('x');
 });
+
+
+app.get('/test', async (request, response) => {
+    for (let i = 0; i < x.russell3000X.length; i++) { 
+        await axios.get(`https://api.intrinio.com/financials/standardized?identifier=${x.russell3000X[i]}&statement=calculations&type=QTR`,
+            {
+                headers: {'Authorization': auth}
+            }).then(response => {
+                console.log(x.russell3000X[i],'!',response.data.data);
+            }).catch(() => {
+                console.log('null could not get');
+        });   
+    }
+response.send('x');
+});
+
 
 app.listen(8000, () => {
     console.log('listening on port 8000');
